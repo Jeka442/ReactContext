5524cf1e79d2dd0de4aeac61c4b4781e3835a8f6
Eivgeniy Gudilov <85572701+Jeka442@users.noreply.github.com>
2022-07-24 23:10:40 +0300

    Update index.tsx

--- src/Modules/index.tsx
+++ src/Modules/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { CounterLayout } from "../Components";
+import { CounterLayout } from "../Components/CounterLayout";
 import { MyContextProvider } from "../Context/MyContextProvider";
 
 export const Page = () => {
